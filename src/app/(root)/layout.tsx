f473aa07ca0fbe661dbf177ae2b678ee47faf9f8
Evar-SF27 <evarfomuso@outlook.com>
2024-06-07 14:45:51 +0100

    leftsidebar responsive

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,12 +1,26 @@
+import MobileSideBar from "@/components/sidebar/mobile";
+import Sidebar from "@/components/sidebar/sidebar"
+import Image from "next/image" 
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Evar', lastName: 'Fomuso' }
+
   return (
     <main>
-        SIDEBAR
+        <Sidebar user={loggedIn} />
+        <div className='flex size-full flex-col'>
+          <div className='root-layout'>
+            <Image src='/icons/logo.svg' width={30} height={30} alt='menu icon' />
+            <div>
+              <MobileSideBar user={loggedIn} />
+            </div>
+          </div>
         {children}
+        </div>
     </main>
   );
 }
